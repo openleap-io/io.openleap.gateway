f2ee5a8e8c312cc165a8204831e2a9a99db82327
Kiril Arsov <kirilarsov@gmail.com>
2025-04-08 11:49:28 +0200

    ga profile

--- src/main/java/io/openleap/gateway/config/KeycloakConfig.java
+++ src/main/java/io/openleap/gateway/config/KeycloakConfig.java
@@ -28,7 +28,7 @@ public class KeycloakConfig {
     private final OAuth2ClientProperties clientProperties;
     private final ClientRegistrationProperties clientRegistrationProperties;
     private final EurekaInstanceConfigBean eurekaInstanceConfigBean;
-    String[] allowedServices = {"/identity/**", "/actuator/**", "/api/**"};
+    String[] allowedServices = {"/identity/**", "/actuator/**", "/ga/**"};
 
     public KeycloakConfig(OAuth2ClientProperties clientProperties, ClientRegistrationProperties clientRegistrationProperties, EurekaInstanceConfigBean eurekaInstanceConfigBean) {
         this.clientProperties = clientProperties;
